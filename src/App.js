7be49ce4c9867f1669373ce5fe0f2c3a4ab3a12a
Valentin Culasso <val_nic@hotmail.com.ar>
2024-10-31 17:59:32 -0300

    pageJuegos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import NavBarComponent from './components/NavBarComponent';
+import JuegoPage from './pages/juegoPage';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <NavBarComponent />
       <Routes>
         <Route path="/" element={<h2>Bienvenido a Videojuegos</h2>} />
-        <Route path="/juegos" element={<h2>Listado de Juegos</h2>} />
+        <Route path="/juegos" element={<JuegoPage/>} />
         <Route path="/registro" element={<h2>Registro</h2>} />
         <Route path="/login" element={<h2>Login</h2>} />
       </Routes>
@@ -19,6 +20,8 @@ function App() {
     </Router>
   );
 }
+
+// Fetch nativo javascript , axios dependencia
 
 
 export default App;
